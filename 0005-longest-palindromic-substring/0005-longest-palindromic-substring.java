8ffb97139950e3d10b22070da0ffceff4996f040
Kesav4ncodez <kesav4niq@gmail.com>
2026-02-13 14:05:42 +0530

    Time: 2523 ms (5.01%), Space: 45.6 MB (45.01%) - LeetHub

---
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -0,0 +1,23 @@
+class Solution {
+    public String longestPalindrome(String s) {
+        String longest="";
+        for(int i=0;i<s.length();i++){
+            for(int j=i;j<s.length();j++){
+                String sub=s.substring(i,j+1);
+                if(ispalindrome(sub) && sub.length()>longest.length()) longest=sub;
+            }
+        }
+        return longest;
+    }
+    
+    public boolean ispalindrome(String s){
+    int left=0;
+    int right=s.length()-1;
+    while(left<right){
+        if(s.charAt(left)!=s.charAt(right)) return false;
+        left++;
+        right--;
+    }
+    return true;
+    }
+}
